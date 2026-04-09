5e8a2a09856d2953838756be7286707a87a309fc
jasonruiz <jason.ruiz@melonlocal.com>
2026-04-09 15:30:44 -0600

    Added grid template areas for two sidebars, a header, footer, and main. Changed the main grid to use display grid as well with auto adjusting columns.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,24 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<div class="layoutContainer">
+
+<header class="layoutItem" style="grid-area: header">
+	<h1> Svelte Cafe</h1>
+</header>
+
+<aside class="layoutItem" style="grid-area: left-sidebar">
+
+</aside>
+<section class="layoutItem" style="grid-area: main">
 {@render children()}
+</section>
+<aside class="layoutItem" style="grid-area: right-sidebar">
+
+</aside>
+<footer class="layoutItem" style="grid-area: footer">
+	Published by Jason Ruiz 2026
+</footer>
+</div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,20 +9,17 @@ let cart = $state<CartItem[]>([]);
 </script>
 
 
-<div>
-
-
-    <div class="grid grid-cols-1 gap-6 bg-gray-100 p-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div class="gridContainer">
         {#each data.menuItems as item (item.id)}
 
-        <div class="overflow-hidden rounded-xl bg-white shadow-lg">
+        <div class="gridItem">
 			<!-- <img src={product.thumbnail} alt={product.title} class="h-48 w-full object-cover" /> -->
 			<div class="p-4">
 				<p class="mb-2 overflow-hidden truncate text-lg font-medium text-gray-800">
 					{item.name}
 				</p>
 				<div class="flex items-center justify-between">
-					<p class="text-xl font-bold">${item.price}</p>
+					<p class="text-xl font-bold text-cyan-700">${item.price}</p>
 					<button
 						class="rounded-full bg-sky-600 px-4 py-2 text-white transition-colors duration-300 hover:bg-sky-700"
                         onclick={() => {
@@ -37,5 +34,4 @@ let cart = $state<CartItem[]>([]);
 			</div>
 		</div>
         {/each}
-    </div>
     </div>
